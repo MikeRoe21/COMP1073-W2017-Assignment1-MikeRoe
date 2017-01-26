38f9a5fd3fa60b19ac1b894c9328e4357f655c74
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2017-01-26 14:28:58 -0500

    first attempt at JS and CSS

--- Desktop/Client-Side-Scripting-Assignment-1/Scripts/app.js
+++ Desktop/Client-Side-Scripting-Assignment-1/Scripts/app.js
@@ -1,1 +1,7 @@
 /* JavaScript goes here */
+
+//First test injecting JavaScript textural content into my index.html
+
+var FirstParagraph = document.getElementById("FirstParagraph");
+var myContent = "My name is Mike Roe. I am a first year student just trying to get my name out there.";
+FirstParagraph.textContent = myContent;

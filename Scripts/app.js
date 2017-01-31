1a7c98c4da6ce34fa513a2351ac8ac1d9a29635d
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2017-01-31 15:03:08 -0500

    Final Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,13 +1,11 @@
-/* JavaScript goes here */
-
-//First test injecting JavaScript textural content into my index.html
+/* Using JavaScript to write the text on the main page */
 
 var FirstParagraph = document.getElementById("FirstParagraph");
 var Intro = "My name is Mike Roe. I am a first year student just trying to get my name out there.";
 FirstParagraph.textContent = Intro;
 
 var SecondParagraph = document.getElementById("SecondParagraph");
-var Photo = "Here is a photo from last April of my graduating class in Haliburton, Ontario. I am in the back with the grey shirt and long hair";
+var Photo = "My Graduating class from last April. I am in the back, 2nd from the left with the long hair.";
 SecondParagraph.textContent = Photo;
 
 
